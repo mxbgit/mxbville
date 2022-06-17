9b9d982d7ea561ca8d3ffd19bda115c641b20cab
mxbgit <maxim.balaganskij@gmx.de>
2022-06-18 00:18:38 +0200

    Fixed Hardness of step block

--- src/main/java/mxbville/common/blocks/decorative/BlockStep.java
+++ src/main/java/mxbville/common/blocks/decorative/BlockStep.java
@@ -2,6 +2,7 @@ package mxbville.common.blocks.decorative;
 
 import mxbville.common.blocks.misc.BlockFacing;
 import net.minecraft.block.BlockFence;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyBool;
@@ -31,6 +32,8 @@ public class BlockStep extends BlockFacing {
 
 	public BlockStep(String name) {
 		super(name, Material.WOOD);
+		this.setHardness(2.5F);
+        this.setSoundType(SoundType.WOOD);
 		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH).withProperty(ATTACHED, Boolean.valueOf(false)));
 	}
 	
